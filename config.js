48b5961fb9d8ff1f3dc6f35374dc46f66ff3c32f
Tetsuya SATO <tetsuyas1@gmail.com>
2021-03-08 16:09:02 +0900

    first

---
+++ config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":1,"title":"","author":"馬場　俊介","subject":"","keywords":"","creator":"Microsoft® Word 2016","producer":"Microsoft® Word 2016","creationdate":"D:20210226172713+09'00'","moddate":"D:20210226172713+09'00'","trapped":"","fileName":"_______RCL__PP______.pdf","bounds":[[909,1286]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
